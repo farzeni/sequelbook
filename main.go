16c8a07599f0189076e131d410211660a8b44d65
Fabrizio Arzeni <fabrizio.arz@gmail.com>
2024-09-26 08:59:09 +0200

    feat: Update window size and add Linux options
    Update the window size to 1440x900 and add Linux options for window transparency, GPU policy, and program name. Also enable the debug mode to open the inspector on startup.

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/linux"
 )
 
 //go:embed all:frontend/dist
@@ -20,18 +21,26 @@ func main() {
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "SequelBook",
-		Width:  1280,
-		Height: 800,
+		Width:  1440,
+		Height: 900,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
 		OnStartup: core.Startup,
-		Frameless: true,
+		// Frameless: true,
 		Bind: []interface{}{
 			core,
 			core.Connections,
 			core.Books,
 			core.Pooler,
+		},
+		Linux: &linux.Options{
+			WindowIsTranslucent: false,
+			WebviewGpuPolicy:    linux.WebviewGpuPolicyOnDemand,
+			ProgramName:         "sequelbook",
+		},
+		Debug: options.Debug{
+			OpenInspectorOnStartup: true,
 		},
 	})
 
