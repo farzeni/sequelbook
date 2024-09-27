38f0d225f5d257eb83f827135e48316f4795701e
Fabrizio Arzeni <fabrizio.arz@gmail.com>
2024-09-27 08:51:38 +0200

    perf: improve cell list rendering

--- frontend/src/app/Book/Cells/TextCell/index.tsx
+++ frontend/src/app/Book/Cells/TextCell/index.tsx
@@ -22,7 +22,7 @@ import {
 } from '@mdxeditor/editor';
 import '@mdxeditor/editor/style.css';
 import { SaveIcon } from "lucide-react";
-import { FC, useEffect, useRef, useState } from "react";
+import { FC, useCallback, useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 import MarkdownView from 'react-showdown';
 import CellMenu from "../CellMenu";
@@ -32,39 +32,44 @@ interface TextBlockProps {
   bookId: string
   cell: books.Cell
   selected?: boolean
-  onChange?: (content: string) => void
+  onChange?: (cellId: string, content: string) => void
 }
 
 const TextBlock: FC<TextBlockProps> = ({ bookId, cell, selected, onChange }) => {
   const { t } = useTranslation()
   const [editMode, setEditMode] = useState(false)
+  const [isMouseOver, setIsMouseOver] = useState(false)
 
   const editorRef = useRef<MDXEditorMethods>(null)
 
   const debouncedOnChange = useDebounce((content: string) => {
-    onChange && onChange(content)
+    onChange && onChange(cell.id, content)
   }, EDITOR_DEBOUNCE)
 
-  function handleContentChange(content: string) {
-    debouncedOnChange(content)  // Use the debounced version of onChange
-  }
 
-  function handleKeyDown(e: KeyboardEvent) {
-    console.log("e.key", e.key)
-    if (e.key === "Control-" + CELL_BINDINGS.CELL_NEXT) {
+  const handleKeyDown = useCallback((e: KeyboardEvent) => {
+    const isCtrlOrCmd = e.ctrlKey || e.metaKey;
+
+    if (isCtrlOrCmd && e.key === CELL_BINDINGS.CELL_NEXT) {
       e.preventDefault()
       SelectNextCell()
     }
 
-    if (e.key === "Control-" + CELL_BINDINGS.CELL_PREVIOUS) {
+    if (isCtrlOrCmd && e.key === CELL_BINDINGS.CELL_PREVIOUS) {
       e.preventDefault()
       SelectPreviousCell()
     }
-  }
+
+    if (isCtrlOrCmd && e.key === "Enter") {
+      e.preventDefault()
+      e.stopPropagation()
+      setEditMode(false)
+    }
+  }, [])
 
   useEffect(() => {
     if (editMode == false && editorRef.current) {
-      onChange && onChange(editorRef.current?.getMarkdown())
+      onChange && onChange(cell.id, editorRef.current?.getMarkdown())
     }
   }, [editMode])
 
@@ -81,11 +86,10 @@ const TextBlock: FC<TextBlockProps> = ({ bookId, cell, selected, onChange }) => 
     }
   }, [selected])
 
-
-
   return (
-    <div className="w-full prose mx-auto relative" onDoubleClick={() => !editMode && setEditMode(true)} >
-      {selected && (
+    <div className="w-full prose mx-auto relative" onDoubleClick={() => !editMode && setEditMode(true)}
+      onMouseEnter={() => setIsMouseOver(true)} onMouseLeave={() => setIsMouseOver(false)}>
+      {(selected || isMouseOver) && (
         <CellMenu cell={cell} bookId={bookId}>
           <div className="flex justify-center items-center w-[30px] cursor-pointer" onClick={() => setEditMode(false)}>
             <SaveIcon size={18} />
@@ -124,7 +128,7 @@ const TextBlock: FC<TextBlockProps> = ({ bookId, cell, selected, onChange }) => 
           ]}
           ref={editorRef}
           markdown={cell.content}
-          onChange={handleContentChange} />
+          onChange={debouncedOnChange} />
 
       ) :
         cell.content.length > 0 ? (
@@ -135,7 +139,7 @@ const TextBlock: FC<TextBlockProps> = ({ bookId, cell, selected, onChange }) => 
           <div className="py-2 text-gray-500">{t("doubleClickToEdit", "Double click this cell to edit")}</div>
         )
       }
-    </div>
+    </div >
   )
 }
 
--- frontend/src/app/Book/Cells/index.tsx
+++ frontend/src/app/Book/Cells/index.tsx
@@ -1,6 +1,6 @@
 import { SetSelectedCell, UpdateCell, useStore } from "@hooks/store";
 import { books } from "@lib/wailsjs/go/models";
-import { FC } from "react";
+import { FC, useCallback, useMemo } from "react";
 import CodeBlock from "./CodeCell";
 import QuickAdd from "./QuickAdd";
 import TextBlock from "./TextCell";
@@ -10,40 +10,56 @@ interface CellsListProps {
 }
 
 const CellsList: FC<CellsListProps> = ({ book }) => {
-  const tab = useStore((state) => state.editor.currentBookId ? state.editor.tabs[state.editor.currentBookId] : null)
-
-  async function handleBookChange(cellId: string, source: string) {
-    if (!book) return
-    await UpdateCell(book.id, cellId, source)
-  }
-
   return (
     <div>
       <QuickAdd index={0} />
 
       {book.cells.map((cell: books.Cell, idx: number) => (
-        <div key={cell.id}
-          onClick={() => SetSelectedCell(cell.id)}
-          className={`
+
+        <CellsListItem idx={idx} book={book} cell={cell} />
+      ))
+      }
+    </div >
+  )
+
+}
+
+const CellsListItem: FC<{ idx: number, book: books.Book, cell: books.Cell }> = ({ idx, book, cell }) => {
+  const selectedCellId = useStore((state) => state.editor.currentBookId ? state.editor.tabs[state.editor.currentBookId]?.cellId : null)
+
+  const handleBookChange = useCallback(async (cellId: string, source: string) => {
+    if (!book) return
+    UpdateCell(book.id, cellId, source)
+  }, [book])
+
+  const bookId = useMemo(() => book.id, [book])
+  const cellMemo = useMemo(() => cell, [cell])
+  const selected = useMemo(() => {
+    return selectedCellId === cell.id
+  }, [selectedCellId, cell])
+
+  return (
+    <div key={cell.id}
+      onClick={() => SetSelectedCell(cell.id)}
+      className={`
           max-w-screen-lg
           mx-auto
           relative 
           rounded 
-          ${tab?.cellId === cell.id ? "border-slate-600" : ""}
+          ${selected ? "border-slate-600" : ""}
           
         `}>
-          {cell.type === "code" && (
-            <CodeBlock bookId={book.id} cell={cell} onChange={(source) => handleBookChange(cell.id, source)} selected={tab?.cellId === cell.id} />
-          )}
 
-          {cell.type === "text" && (
-            <TextBlock bookId={book.id} cell={cell} onChange={(source) => handleBookChange(cell.id, source)} selected={tab?.cellId === cell.id} />
-          )}
-          <QuickAdd index={idx + 1} />
-        </div>
-      ))
-      }
-    </div >
+      {cell.type === "code" && (
+        <CodeBlock bookId={bookId} cell={cellMemo} onChange={handleBookChange} selected={selected} />
+      )}
+
+      {cell.type === "text" && (
+        <TextBlock bookId={bookId} cell={cellMemo} onChange={handleBookChange} selected={selected} />
+      )}
+
+      <QuickAdd index={idx + 1} />
+    </div>
   )
 
 }
